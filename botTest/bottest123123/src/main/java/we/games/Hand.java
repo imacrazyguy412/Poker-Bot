d5051d8e2a855ca856cc64a5e3efe1ba369d913f
imacrazyguy412 <110923269+imacrazyguy412@users.noreply.github.com>
2022-11-16 09:30:51 -0500

    Create Hand objects
    ur mom

---
+++ botTest/bottest123123/src/main/java/we/games/Hand.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Hand{
+  private ArrayList<Card> cards;
+  
+  public Hand(){
+    cards = new ArrayList<Card>;
+  }
+  
+  public Hand(ArrayList<Card> h){
+    cards = new ArrayList<Card>;
+    
+    for(int i = 0; i < h.Size(); i++){
+      cards.add(h.get(i));
+    }
+    //Im not sure if I need this for loop, but Im gonna do it anyway
+  }
+  
+  public ArrayList<Card> getCards(){
+    return cards;
+  }
+  
+  public Card getCard(int index){
+    return cards.get(index);
+  }
+  public void sort(){
+  }
+}

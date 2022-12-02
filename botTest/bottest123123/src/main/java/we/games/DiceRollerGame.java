1fcef36725f29e83f1d7ecd8fe9633240a309d9a
Something-Inconspicuous <117416222+Something-Inconspicuous@users.noreply.github.com>
2022-12-02 09:31:49 -0500

    added the basics of the dice rolling game
    added code that we can use for the dice rolling game. should be simple

--- botTest/bottest123123/src/main/java/we/games/DiceRollerGame.java
+++ botTest/bottest123123/src/main/java/we/games/DiceRollerGame.java
@@ -1,1 +1,38 @@
 package we.games;
+
+import java.util.Random; //TODO: when we do multithreading, maybe change to local thread random
+
+public class DiceRollerGame{
+  private int roll1, roll2
+  private int total;
+  private String playerName;
+  
+  public DiceRollerGame(String name){
+    playerName = name;
+  }
+  
+  public void play(){
+    roll1 = java.util.Random.nextInt(6) + 1;
+    roll2 = java.util.Random.nextInt(6) + 1;
+    
+    total = roll1 + roll2;
+  }
+  
+  public int getTotal(){
+    return total;
+  }
+  
+  public String getPlayerName(){
+    return playerName;
+  }
+  
+  @Override
+  public boolean equals(Object obj){
+    DiceRollerGame d = (DiceRollerGame)obj;
+    
+    if(d.getPlayerName().equals(this.getPlayerName())){
+      return true;
+    }
+    return false;
+  }
+}
